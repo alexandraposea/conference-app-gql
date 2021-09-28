3e0a545154df09fe562dd12befcdb89a976f245f
alexandraposea98@yahoo.com <alexandraposea98@yahoo.com>
2021-09-28 16:00:46 +0300

    add helloWorld

---
+++ src/features/helloWorld/resolvers.js
@@ -0,0 +1,9 @@
+const helloWorldResolvers = {
+  Query: {
+    myFirstEndpoint: async (_parent, _arguments, _context, _info) => {
+      return 'Hello World!🐱‍👤 This is coming from GraphQL server! 👌🤘🏻'
+    }
+  }
+}
+
+module.exports = helloWorldResolvers
